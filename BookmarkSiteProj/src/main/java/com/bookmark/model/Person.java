6902d5f1fa376a73bda7086ed49d592a2260d12a
Toshihiro Murata <eb68276@jp.ibm.com>
2015-04-24 17:39:49 +0900

    Add some changes.

--- BookmarkSiteProj/src/main/java/com/bookmark/model/Person.java
+++ BookmarkSiteProj/src/main/java/com/bookmark/model/Person.java
@@ -1,13 +1,21 @@
 package com.bookmark.model;
 
 public class Person {
-    private final String name;
+	
+	private final String name;
+	private final long id;
 
-    public Person(String name) {
-        this.name = name;
-    }
+	public Person(String name, long id) {
+		this.name = name;
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
 }
--- BookmarkSiteProj/src/test/java/com/bookmark/model/PersonTest.java
+++ BookmarkSiteProj/src/test/java/com/bookmark/model/PersonTest.java
@@ -1,12 +1,22 @@
 package com.bookmark.model;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PersonTest {
     @Test
-    public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
+    public void canConstructAPersonWithANameAndId() {
+    	AtomicLong counter = new AtomicLong();
+    	
+        Person person = new Person("Larry", counter.getAndIncrement());
         assertEquals("Larry", person.getName());
+        assertEquals(0, person.getId());
+        
+        person = new Person("Bobbie", counter.getAndIncrement());
+        assertEquals("Bobbie", person.getName());
+        assertEquals(1, person.getId());
     }
 }
