41430faea3d5267bbb731b684654e8b5b42d6ff2
Toshihiro Murata <eb68276@jp.ibm.com>
2015-04-24 18:10:29 +0900

    Add Bookmarks properies in Person class, and a test for that.

--- BookmarkSiteProj/src/main/java/com/bookmark/model/Person.java
+++ BookmarkSiteProj/src/main/java/com/bookmark/model/Person.java
@@ -1,9 +1,12 @@
 package com.bookmark.model;
 
+import java.util.ArrayList;
+
 public class Person {
 	
 	private final String name;
 	private final long id;
+	private ArrayList<Bookmark> bookmarks = new ArrayList<Bookmark>();
 
 	public Person(String name, long id) {
 		this.name = name;
@@ -16,6 +19,18 @@ public class Person {
 
 	public long getId() {
 		return id;
+	}
+
+	public void addBookmark(Bookmark bookmark) {
+		bookmarks.add(bookmark);
+	}
+
+	public ArrayList<Bookmark> getBookmarks() {
+		return bookmarks;
+	}
+
+	public Bookmark getBookmark(int index) {
+		return bookmarks.get(index);
 	}
 
 }
--- BookmarkSiteProj/src/test/java/com/bookmark/model/PersonTest.java
+++ BookmarkSiteProj/src/test/java/com/bookmark/model/PersonTest.java
@@ -1,5 +1,6 @@
 package com.bookmark.model;
 
+import java.net.MalformedURLException;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
@@ -8,7 +9,7 @@ import static org.junit.Assert.*;
 
 public class PersonTest {
     @Test
-    public void canConstructAPersonWithANameAndId() {
+    public void canConstructAPersonWithANameAndId() throws MalformedURLException {
     	AtomicLong counter = new AtomicLong();
     	
         Person person = new Person("Larry", counter.getAndIncrement());
@@ -18,5 +19,12 @@ public class PersonTest {
         person = new Person("Bobbie", counter.getAndIncrement());
         assertEquals("Bobbie", person.getName());
         assertEquals(1, person.getId());
+        
+        person.addBookmark(new Bookmark("http://www.yahoo.co.jp", 1));
+        person.addBookmark(new Bookmark("http://www.sega.co.jp", 2));
+        person.addBookmark(new Bookmark("http://www.yahoo.com", 3));
+        assertEquals(3, person.getBookmarks().size());
+        assertEquals(2, person.getBookmark(1).getId());
+        assertEquals("http://www.yahoo.com", person.getBookmark(2).getUrl());
     }
 }
