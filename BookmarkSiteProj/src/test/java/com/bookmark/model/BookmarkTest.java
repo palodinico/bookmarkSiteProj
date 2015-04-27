f5bce25dd98409e4f4143bbd27a6a8ab6676103a
Toshihiro Murata <eb68276@jp.ibm.com>
2015-04-27 11:21:28 +0900

    Add a test for getHost().

--- BookmarkSiteProj/src/test/java/com/bookmark/model/BookmarkTest.java
+++ BookmarkSiteProj/src/test/java/com/bookmark/model/BookmarkTest.java
@@ -13,14 +13,14 @@ public class BookmarkTest {
 	public void canConstructABookmarkWithUrlAndId() throws MalformedURLException {
 		AtomicLong counter = new AtomicLong();
 		
-		Bookmark bookmrk= new Bookmark("http://w3.ibm.com/jp/", counter.getAndIncrement());
-		assertEquals("http://w3.ibm.com/jp/", bookmrk.getUrl());
-		assertEquals(0, bookmrk.getId());
+		Bookmark bookmark= new Bookmark("http://w3.ibm.com/jp/", counter.getAndIncrement());
+		assertEquals("http://w3.ibm.com/jp/", bookmark.getUrl());
+		assertEquals(0, bookmark.getId());
 		
-		bookmrk= new Bookmark("http://www.google.co.jp/", counter.getAndIncrement());
-		assertEquals("http://www.google.co.jp/", bookmrk.getUrl());
-		assertEquals(1, bookmrk.getId());
-		assertEquals("www.google.co.jp", bookmrk.getUrlObj().getHost());
+		bookmark= new Bookmark("http://www.google.co.jp/", counter.getAndIncrement());
+		assertEquals("http://www.google.co.jp/", bookmark.getUrl());
+		assertEquals(1, bookmark.getId());
+		assertEquals("www.google.co.jp", bookmark.getUrlObj().getHost());
 	}
 	
 	@Test(expected=MalformedURLException.class)
