2d364dff12b208816aee2d449c602d6e29461f3b
Toshihiro Murata <eb68276@jp.ibm.com>
2015-04-27 16:31:47 +0900

    Add classes for pagination in com.bookmark.paginate package.

---
+++ BookmarkSiteProj/src/main/java/com/bookmark/paginate/Page.java
@@ -0,0 +1,35 @@
+package com.bookmark.paginate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Page<E> {
+	
+	private int pageNumber;
+	private int pageAvailable;
+	private List<E> pageItems = new ArrayList<E>();
+	
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+	
+	public void setPageAvaliable(int pageAvailable) {
+		this.pageAvailable = pageAvailable;
+	}
+	
+	public void setPageItems(List<E> pageItems) {
+		this.pageItems = pageItems;
+	}
+	
+	public int getPageNumber() {
+		return pageNumber;
+	}
+	
+	public int getPageAvailable() {
+		return pageAvailable;
+	}
+	
+	public List<E> getPageItems() {
+		return pageItems;
+	}
+}
---
+++ BookmarkSiteProj/src/main/java/com/bookmark/paginate/PaginationHelper.java
@@ -0,0 +1,59 @@
+package com.bookmark.paginate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+
+public class PaginationHelper<E> {
+	
+	public Page<E> fetchPage (
+			final JdbcTemplate jt,
+			final String sqlCountRows,
+			final String sqlFetchRows,
+			final Object args[],
+			final int pageNo,
+			final int pageSize,
+			final ParameterizedRowMapper<E> rowMapper) {
+		
+		final int rowCount = jt.queryForInt(sqlCountRows, args);
+		
+		int pageCount = rowCount / pageSize;
+		if (rowCount > pageSize * pageCount) {
+			pageCount++;
+		}
+		
+		final Page<E> page = new Page<E>();
+		page.setPageNumber(pageNo);
+		page.setPageAvaliable(pageCount);
+		
+		final int startRow = (pageNo - 1) * pageSize;
+		jt.query(
+				sqlFetchRows, 
+				args, 
+				new ResultSetExtractor(){
+
+					@Override
+					public Object extractData(ResultSet rs)
+							throws SQLException, DataAccessException {
+						final List pageItems = page.getPageItems();
+						int currentRow = 0;
+						while (rs.next() && currentRow < startRow + pageSize) {
+							if (currentRow >= startRow) {
+								pageItems.add(rowMapper.mapRow(rs, currentRow));
+							}
+							currentRow++;
+						}
+						return page;
+					}
+					
+				});
+		
+		return page;
+	}
+
+}
