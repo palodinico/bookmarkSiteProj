91b510a98a83fda3c49b9fa924cbc8f2655705c7
Toshihiro Murata <eb68276@jp.ibm.com>
2015-04-28 18:05:37 +0900

    Add a function to show pagination by number texts.

--- BookmarkSiteProj/src/main/java/com/bookmark/controller/BookmarkController.java
+++ BookmarkSiteProj/src/main/java/com/bookmark/controller/BookmarkController.java
@@ -3,6 +3,7 @@ package com.bookmark.controller;
 import java.net.MalformedURLException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,9 @@ public class BookmarkController {
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
+	private final int pageNumberDispayNum = 7;
+
 	@RequestMapping("/bookmarks")
 	public String bookmarks(Model model) {
 		model.addAttribute("title", "All bookarks");
@@ -38,12 +41,12 @@ public class BookmarkController {
 
 	@RequestMapping("/addSampleData")
 	public String addSampleData(
-			@RequestParam(value="pageNumber", required=false, defaultValue="1") int pageNumber,
-			@RequestParam(value="itemsPerPage", required=false, defaultValue="10") int itemsPerPage,
+			@RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber,
+			@RequestParam(value = "itemsPerPage", required = false, defaultValue = "10") int itemsPerPage,
 			Model model) {
-		
+
 		Page<Bookmark> bookmarks = getBookmarks(pageNumber, itemsPerPage);
-		
+
 		model.addAttribute("bookmarks", bookmarks.getPageItems());
 		model.addAttribute("pageNumber", bookmarks.getPageNumber());
 		model.addAttribute("pageAvailable", bookmarks.getPageAvailable());
@@ -51,29 +54,56 @@ public class BookmarkController {
 		if (bookmarks.getPageNumber() == 1) {
 			model.addAttribute("isFistPage", true);
 		}
-		if ( bookmarks.getPageAvailable() <= bookmarks.getPageNumber()) {
+		if (bookmarks.getPageAvailable() <= bookmarks.getPageNumber()) {
 			model.addAttribute("isLastPage", true);
 		}
-		
+		model.addAttribute(
+				"pageNumbers",
+				getPageNumbers(bookmarks.getPageNumber(),
+						bookmarks.getPageAvailable()));
+
 		return "addSampleData";
 	}
-	
+
+	private final ArrayList<Integer> getPageNumbers(int pageNumber, int pageAvailable) {
+		int allPageNumbers = pageNumber + (pageNumberDispayNum/2);
+		if (allPageNumbers < pageNumberDispayNum) {
+			allPageNumbers = pageNumberDispayNum;
+		}
+		if (allPageNumbers > pageAvailable) {
+			allPageNumbers = pageAvailable;
+		}
+		int startPageNumber = pageNumber - (pageNumberDispayNum/2);
+		if (startPageNumber < 1) {
+			startPageNumber = 1;
+		}
+		
+		if (pageNumber > (allPageNumbers - pageNumberDispayNum)) {
+			startPageNumber = allPageNumbers - pageNumberDispayNum + 1;
+		}
+		if (startPageNumber < 1) {
+			startPageNumber = 1;
+		}
+		
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		for (int i = startPageNumber; i <= allPageNumbers; i++) {
+			numbers.add(i);
+		}
+		return numbers;
+	}
+
 	public Page<Bookmark> getBookmarks(final int pageNo, final int pageSize) {
 		PaginationHelper<Bookmark> ph = new PaginationHelper<Bookmark>();
-		return ph.fetchPage(
-				jdbcTemplate, 
-				"SELECT count(*) FROM bookmarks", 
-				"SELECT id, url FROM bookmarks", 
-				new Object[]{}, 
-				pageNo, 
-				pageSize, 
-				new ParameterizedRowMapper<Bookmark>(){
+		return ph.fetchPage(jdbcTemplate, "SELECT count(*) FROM bookmarks",
+				"SELECT id, url FROM bookmarks", new Object[] {}, pageNo,
+				pageSize, new ParameterizedRowMapper<Bookmark>() {
 					@Override
 					public Bookmark mapRow(ResultSet rs, int rowNum)
 							throws SQLException {
 						Bookmark bookmark = null;
 						try {
-							bookmark = new Bookmark(rs.getString("url"), rs.getLong("id"));
+							bookmark = new Bookmark(rs.getString("url"), rs
+									.getLong("id"));
 						} catch (MalformedURLException e) {
 							e.printStackTrace();
 						}
